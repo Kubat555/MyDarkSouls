5311d31b2f05824fe0b3cb9340cd95f42f85fd1c
@Kubat555 <kubat.chyngyzov@gmail.com>
2022-09-18 00:00:32 +0600

    add player movement scripts with InputSystem

--- MyDarkSouls/Assets/Scripts/PlayerLocomotion.cs
+++ MyDarkSouls/Assets/Scripts/PlayerLocomotion.cs
@@ -2,17 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotion : MonoBehaviour
+namespace MainSpace
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerLocomotion : MonoBehaviour
     {
-        
-    }
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [HideInInspector]
+        public Transform myTransform;
+
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5;
+        [SerializeField]
+        float rotationSpeed = 10;
+
+        private void Start() {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = Camera.main.transform;
+        }
+
+        private void Update() {
+            float delta = Time.deltaTime;
+
+            inputHandler.TickInput(delta);
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelosity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelosity;
+        }
+
+
+        #region Movement
+
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta){
+            Vector3 targetDirection = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDirection = cameraObject.forward * inputHandler.vertical;
+            targetDirection += cameraObject.right * inputHandler.horizontal;
+
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if(targetDirection == Vector3.zero){
+                targetDirection = myTransform.forward;
+            }
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDirection);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+        #endregion
     }
 }
